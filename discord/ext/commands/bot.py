a8801ccc42ae8b6e9a898b049ba11b35482ccd23
Restyled.io <commits@restyled.io>
2021-05-11 16:34:44 +0000

    Restyled by black

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -40,10 +40,10 @@ from .help import HelpCommand, DefaultHelpCommand
 from .cog import Cog
 
 __all__ = (
-    'when_mentioned',
-    'when_mentioned_or',
-    'Bot',
-    'AutoShardedBot',
+    "when_mentioned",
+    "when_mentioned_or",
+    "Bot",
+    "AutoShardedBot",
 )
 
 
@@ -52,7 +52,7 @@ def when_mentioned(bot, msg):
 
     These are meant to be passed into the :attr:`.Bot.command_prefix` attribute.
     """
-    return [f'<@{bot.user.id}> ', f'<@!{bot.user.id}> ']
+    return [f"<@{bot.user.id}> ", f"<@!{bot.user.id}> "]
 
 
 def when_mentioned_or(*prefixes):
@@ -84,6 +84,7 @@ def when_mentioned_or(*prefixes):
     ----------
     :func:`.when_mentioned`
     """
+
     def inner(bot, msg):
         r = list(prefixes)
         r = when_mentioned(bot, msg) + r
@@ -98,14 +99,16 @@ def _is_submodule(parent, child):
 
 class _DefaultRepr:
     def __repr__(self):
-        return '<default-help-command>'
+        return "<default-help-command>"
 
 
 _default = _DefaultRepr()
 
 
 class BotBase(GroupMixin):
-    def __init__(self, command_prefix, help_command=_default, description=None, **options):
+    def __init__(
+        self, command_prefix, help_command=_default, description=None, **options
+    ):
         super().__init__(**options)
         self.command_prefix = command_prefix
         self.extra_events = {}
@@ -116,27 +119,33 @@ class BotBase(GroupMixin):
         self._before_invoke = None
         self._after_invoke = None
         self._help_command = None
-        self.description = inspect.cleandoc(description) if description else ''
-        self.owner_id = options.get('owner_id')
-        self.owner_ids = options.get('owner_ids', set())
-        self.strip_after_prefix = options.get('strip_after_prefix', False)
+        self.description = inspect.cleandoc(description) if description else ""
+        self.owner_id = options.get("owner_id")
+        self.owner_ids = options.get("owner_ids", set())
+        self.strip_after_prefix = options.get("strip_after_prefix", False)
 
-        self.self_bot = options.pop('self_bot', False)
-        self.trusted_users = options.pop('trusted_users', [])
+        self.self_bot = options.pop("self_bot", False)
+        self.trusted_users = options.pop("trusted_users", [])
 
         if self.owner_id and self.owner_ids:
-            raise TypeError('Both owner_id and owner_ids are set.')
+            raise TypeError("Both owner_id and owner_ids are set.")
 
-        if self.owner_ids and not isinstance(self.owner_ids, collections.abc.Collection):
+        if self.owner_ids and not isinstance(
+            self.owner_ids, collections.abc.Collection
+        ):
             raise TypeError(
-                f'owner_ids must be a collection not {self.owner_ids.__class__!r}')
+                f"owner_ids must be a collection not {self.owner_ids.__class__!r}"
+            )
 
         if self.self_bot and self.trusted_users:
-            raise TypeError('Both self_bot and trusted_users are set.')
+            raise TypeError("Both self_bot and trusted_users are set.")
 
-        if self.trusted_users and not isinstance(self.trusted_users, collections.abc.Collection):
+        if self.trusted_users and not isinstance(
+            self.trusted_users, collections.abc.Collection
+        ):
             raise TypeError(
-                f'trusted_users must be a collection not {self.trusted_users.__class__!r}')
+                f"trusted_users must be a collection not {self.trusted_users.__class__!r}"
+            )
 
         if self.trusted_users:
             # skip when the message author is not trusted
@@ -158,7 +167,7 @@ class BotBase(GroupMixin):
 
     def dispatch(self, event_name, *args, **kwargs):
         super().dispatch(event_name, *args, **kwargs)
-        ev = 'on_' + event_name
+        ev = "on_" + event_name
         for event in self.extra_events.get(ev, []):
             self._schedule_event(event, ev, *args, **kwargs)
 
@@ -187,7 +196,7 @@ class BotBase(GroupMixin):
 
         This only fires if you do not specify any listeners for command error.
         """
-        if self.extra_events.get('on_command_error', None):
+        if self.extra_events.get("on_command_error", None):
             return
 
         command = context.command
@@ -198,10 +207,10 @@ class BotBase(GroupMixin):
         if cog and cog.has_error_handler():
             return
 
-        print(
-            f'Ignoring exception in command {context.command}:', file=sys.stderr)
+        print(f"Ignoring exception in command {context.command}:", file=sys.stderr)
         traceback.print_exception(
-            type(exception), exception, exception.__traceback__, file=sys.stderr)
+            type(exception), exception, exception.__traceback__, file=sys.stderr
+        )
 
     # global check registration
 
@@ -384,7 +393,7 @@ class BotBase(GroupMixin):
             The coroutine passed is not actually a coroutine.
         """
         if not asyncio.iscoroutinefunction(coro):
-            raise TypeError('The pre-invoke hook must be a coroutine.')
+            raise TypeError("The pre-invoke hook must be a coroutine.")
 
         self._before_invoke = coro
         return coro
@@ -417,7 +426,7 @@ class BotBase(GroupMixin):
             The coroutine passed is not actually a coroutine.
         """
         if not asyncio.iscoroutinefunction(coro):
-            raise TypeError('The post-invoke hook must be a coroutine.')
+            raise TypeError("The post-invoke hook must be a coroutine.")
 
         self._after_invoke = coro
         return coro
@@ -449,7 +458,7 @@ class BotBase(GroupMixin):
         name = func.__name__ if name is None else name
 
         if not asyncio.iscoroutinefunction(func):
-            raise TypeError('Listeners must be coroutines')
+            raise TypeError("Listeners must be coroutines")
 
         if name in self.extra_events:
             self.extra_events[name].append(func)
@@ -545,15 +554,14 @@ class BotBase(GroupMixin):
         """
 
         if not isinstance(cog, Cog):
-            raise TypeError('cogs must derive from Cog')
+            raise TypeError("cogs must derive from Cog")
 
         cog_name = cog.__cog_name__
         existing = self.__cogs.get(cog_name)
 
         if existing is not None:
             if not override:
-                raise discord.ClientException(
-                    f'Cog named {cog_name!r} already loaded')
+                raise discord.ClientException(f"Cog named {cog_name!r} already loaded")
             self.remove_cog(cog_name)
 
         cog = cog._inject(self)
@@ -626,7 +634,9 @@ class BotBase(GroupMixin):
         for event_list in self.extra_events.copy().values():
             remove = []
             for index, event in enumerate(event_list):
-                if event.__module__ is not None and _is_submodule(name, event.__module__):
+                if event.__module__ is not None and _is_submodule(
+                    name, event.__module__
+                ):
                     remove.append(index)
 
             for index in reversed(remove):
@@ -634,7 +644,7 @@ class BotBase(GroupMixin):
 
     def _call_module_finalizers(self, lib, key):
         try:
-            func = getattr(lib, 'teardown')
+            func = getattr(lib, "teardown")
         except AttributeError:
             pass
         else:
@@ -661,7 +671,7 @@ class BotBase(GroupMixin):
             raise errors.ExtensionFailed(key, e) from e
 
         try:
-            setup = getattr(lib, 'setup')
+            setup = getattr(lib, "setup")
         except AttributeError:
             del sys.modules[key]
             raise errors.NoEntryPointError(key)
@@ -848,8 +858,7 @@ class BotBase(GroupMixin):
     def help_command(self, value):
         if value is not None:
             if not isinstance(value, HelpCommand):
-                raise TypeError(
-                    'help_command must be a subclass of HelpCommand')
+                raise TypeError("help_command must be a subclass of HelpCommand")
             if self._help_command is not None:
                 self._help_command._remove_from_bot(self)
             self._help_command = value
@@ -892,12 +901,15 @@ class BotBase(GroupMixin):
                 if isinstance(ret, collections.abc.Iterable):
                     raise
 
-                raise TypeError("command_prefix must be plain string, iterable of strings, or callable "
-                                f"returning either of these, not {ret.__class__.__name__}")
+                raise TypeError(
+                    "command_prefix must be plain string, iterable of strings, or callable "
+                    f"returning either of these, not {ret.__class__.__name__}"
+                )
 
             if not ret:
                 raise ValueError(
-                    "Iterable command_prefix must contain at least one prefix")
+                    "Iterable command_prefix must contain at least one prefix"
+                )
 
         return ret
 
@@ -948,21 +960,24 @@ class BotBase(GroupMixin):
                 # if the context class' __init__ consumes something from the view this
                 # will be wrong.  That seems unreasonable though.
                 if message.content.startswith(tuple(prefix)):
-                    invoked_prefix = discord.utils.find(
-                        view.skip_string, prefix)
+                    invoked_prefix = discord.utils.find(view.skip_string, prefix)
                 else:
                     return ctx
 
             except TypeError:
                 if not isinstance(prefix, list):
-                    raise TypeError("get_prefix must return either a string or a list of string, "
-                                    f"not {prefix.__class__.__name__}")
+                    raise TypeError(
+                        "get_prefix must return either a string or a list of string, "
+                        f"not {prefix.__class__.__name__}"
+                    )
 
                 # It's possible a bad command_prefix got us here.
                 for value in prefix:
                     if not isinstance(value, str):
-                        raise TypeError("Iterable command_prefix or list returned from get_prefix must "
-                                        f"contain only strings, not {value.__class__.__name__}")
+                        raise TypeError(
+                            "Iterable command_prefix or list returned from get_prefix must "
+                            f"contain only strings, not {value.__class__.__name__}"
+                        )
 
                 # Getting here shouldn't happen
                 raise
@@ -988,21 +1003,19 @@ class BotBase(GroupMixin):
             The invocation context to invoke.
         """
         if ctx.command is not None:
-            self.dispatch('command', ctx)
+            self.dispatch("command", ctx)
             try:
                 if await self.can_run(ctx, call_once=True):
                     await ctx.command.invoke(ctx)
                 else:
-                    raise errors.CheckFailure(
-                        'The global check once functions failed.')
+                    raise errors.CheckFailure("The global check once functions failed.")
             except errors.CommandError as exc:
                 await ctx.command.dispatch_error(ctx, exc)
             else:
-                self.dispatch('command_completion', ctx)
+                self.dispatch("command_completion", ctx)
         elif ctx.invoked_with:
-            exc = errors.CommandNotFound(
-                f'Command "{ctx.invoked_with}" is not found')
-            self.dispatch('command_error', ctx, exc)
+            exc = errors.CommandNotFound(f'Command "{ctx.invoked_with}" is not found')
+            self.dispatch("command_error", ctx, exc)
 
     async def process_commands(self, message):
         """|coro|
@@ -1110,6 +1123,7 @@ class Bot(BotBase, discord.Client):
 
         .. versionadded:: 1.7
     """
+
     pass
 
 
@@ -1117,4 +1131,5 @@ class AutoShardedBot(BotBase, discord.AutoShardedClient):
     """This is similar to :class:`.Bot` except that it is inherited from
     :class:`discord.AutoShardedClient` instead.
     """
+
     pass
