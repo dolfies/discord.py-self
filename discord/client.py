369905d49f57ef24504085223633f33c204dccc0
mikasa <alongdreamaway2@gmail.com>
2025-12-07 06:36:34 +0000

    Handle CurlError in Client.connect reconnect loop

--- discord/client.py
+++ discord/client.py
@@ -50,6 +50,7 @@ from typing import (
 )
 
 import aiohttp
+from curl_cffi import CurlError # @mikasa: let Client.connect catch CurlError in reconnect handler :3
 
 from .user import _UserTag, RecentAvatar, User, ClientUser
 from .invite import Invite
@@ -991,6 +992,7 @@ class Client:
                 ConnectionClosed,
                 aiohttp.ClientError,
                 asyncio.TimeoutError,
+                CurlError, # @mikasa: treat CurlError like other reconnectable network errors
             ) as exc:
                 self.dispatch('disconnect')
                 if not reconnect:
