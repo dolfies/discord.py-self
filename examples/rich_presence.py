d1defdf282774a1101db8e19df7a2f07add13d24
Frederick Stempfle <frederickstempfle@MacBook-Pro.local>
2026-02-22 18:37:25 +0100

    Auto-proxy external URLs in activity assets
    change_presence now automatically proxies raw external image URLs
    in activity assets through Discord's media CDN. This means you can
    pass any image URL directly to large_image/small_image on
    ActivityAssets and it just works, as long as the activity has an
    application_id set.
    
    Closes #274

--- examples/rich_presence.py
+++ examples/rich_presence.py
@@ -1,22 +1,17 @@
 import discord
 
-# Your Developer Portal Application ID (used for proxying external assets)
+# Your Developer Portal Application ID
 APPLICATION_ID = 123456789012345678
 
-# Any direct image URLs (png/jpg/gif). Discord will proxy these.
+# Any direct image URLs (png/jpg/gif)
 LARGE_IMAGE_URL = 'image url here'
 SMALL_IMAGE_URL = 'image url here'
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        # Proxy BOTH urls in one call (Discord supports proxying up to 2 at a time).
-        proxied_large, proxied_small = await self.proxy_external_application_assets(
-            APPLICATION_ID,
-            LARGE_IMAGE_URL,
-            SMALL_IMAGE_URL,
-        )
-
+        # External URLs are automatically proxied through Discord's CDN
+        # when passed directly to large_image/small_image.
         activity = discord.Activity(
             type=discord.ActivityType.playing,
             application_id=APPLICATION_ID,
@@ -24,9 +19,9 @@ class MyClient(discord.Client):
             details='details',
             state='state',
             assets=discord.ActivityAssets(
-                large_image=proxied_large,
+                large_image=LARGE_IMAGE_URL,
                 large_text='large_text',
-                small_image=proxied_small,
+                small_image=SMALL_IMAGE_URL,
                 small_text='small_text',
             ),
             buttons=[
